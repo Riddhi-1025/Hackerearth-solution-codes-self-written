78e12077e66ec1e093aefafebaba070004a48f95
Riddhi-1025 <122543274+Riddhi-1025@users.noreply.github.com>
2023-01-22 23:42:16 +0530

    Add files via upload

---
+++ Hackerearth pile of coins.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std; 
+int main() 
+{     
+   int t;     
+   cin>>t;     
+   while(t--)     
+   {         
+   int p,q;         
+   cin>>p>>q;         
+   if(p%2==0 || q%2==0) 
+   cout<<"Ashish"<<endl;         
+   else 
+   cout<<"Jeel"<<endl;              
+   }     
+   return 0; 
+   
+}
